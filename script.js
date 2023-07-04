e4404ec5388a5570c0a05b0e057beb6c3a789265
diego.d <germandebia@hotmail.com>
2023-07-04 02:04:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 let currentItem1 = null;
 let currentItem2 = null;
+let voteCounts = {};
 
-async function getItems() {
-  try {
-    const response = await fetch('/api/items');
-    const items = await response.json();
+// Sample item data
+const items = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  { id: 4, name: 'Item 4' },
+  { id: 5, name: 'Item 5' },
+  // ... Add more items
+];
 
-    if (items.length < 2) {
-      displayTopList();
-      return;
+function initialize() {
+  // Initialize vote counts to zero for each item
+  voteCounts = {};
+  items.forEach(item => {
+    voteCounts[item.id] = 0;
+  });
+
+  getNextItems();
+}
+
+function getNextItems() {
+  const [itemA, itemB] = getRandomItems();
+
+  currentItem1 = itemA;
+  currentItem2 = itemB;
+
+  displayItems();
+}
+
+function getRandomItems() {
+  const randomItems = [];
+  const itemIndices = [];
+
+  // Randomly select two items from the available items
+  while (itemIndices.length < 2 && itemIndices.length < items.length) {
+    const randomIndex = Math.floor(Math.random() * items.length);
+
+    if (!itemIndices.includes(randomIndex)) {
+      itemIndices.push(randomIndex);
+      randomItems.push(items[randomIndex]);
     }
-
-    currentItem1 = items[0];
-    currentItem2 = items[1];
-
-    displayItems();
-  } catch (error) {
-    console.error(error);
   }
+
+  return randomItems;
 }
 
 function displayItems() {
@@ -28,41 +56,19 @@ function displayItems() {
   item2.textContent = currentItem2.name;
 }
 
-async function vote(itemNumber) {
-  try {
-    const winner = itemNumber === 1 ? currentItem1 : currentItem2;
-    const loser = itemNumber === 1 ? currentItem2 : currentItem1;
+function vote(itemNumber) {
+  const winner = itemNumber === 1 ? currentItem1 : currentItem2;
+  const loser = itemNumber === 1 ? currentItem2 : currentItem1;
 
-    await fetch('/api/vote', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ winner, loser }),
-    });
+  // Increment vote count for the winner
+  voteCounts[winner.id] += 1;
 
-    await getItems();
-  } catch (error) {
-    console.error(error);
-  }
+  getNextItems();
+  generateTopList();
 }
 
-async function displayTopList() {
-  try {
-    const response = await fetch('/api/toplist');
-    const topList = await response.json();
+function generateTopList() {
+  const sortedItems = items.slice().sort((a, b) => voteCounts[b.id] - voteCounts[a.id]);
 
-    const listElement = document.getElementById('topList');
-    listElement.innerHTML = '';
-
-    for (const item of topList) {
-      const listItem = document.createElement('li');
-      listItem.textContent = item.name;
-      listElement.appendChild(listItem);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getItems();
+  const topListElement = document.getElementById('topList');
+  topList
